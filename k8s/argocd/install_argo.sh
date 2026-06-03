886dbcdde7a31bf290bcf19c07e035f083557397
Rostyslav <rostyslavpaliuha@gmail.com>
2026-06-03 12:56:20 +0200

    initial refactoring clean up redundant files

--- k8s/argocd/install_argo.sh
+++
@@ -1,7 +0,0 @@
-kubectl create namespace argocd
-kubectl apply --server-side --force-conflicts -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-sleep 10
-echo "admin password: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
-echo "Apply the service application definition"
-kubectl apply -f argocd_service_definition.yml
-kubectl port-forward -n argocd  svc/argocd-server 8082:443
