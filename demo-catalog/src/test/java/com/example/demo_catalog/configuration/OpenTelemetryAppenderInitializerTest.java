7b7e434668fc2be53efba74402201f34aae116fb
Rostyslav <rostyslavpaliuha@gmail.com>
2026-08-14 09:50:59 +0200

    added otel suport with spring boot starter fix

--- demo-catalog/src/test/java/com/example/demo_catalog/configuration/OpenTelemetryAppenderInitializerTest.java
+++ demo-catalog/src/test/java/com/example/demo_catalog/configuration/OpenTelemetryAppenderInitializerTest.java
@@ -2,8 +2,13 @@ package com.example.demo_catalog.configuration;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.LoggingEvent;
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 class OpenTelemetryAppenderInitializerTest {
 
@@ -11,8 +16,24 @@ class OpenTelemetryAppenderInitializerTest {
   void installsOpenTelemetryIntoLogbackAppender() {
     OpenTelemetryAppenderInitializer initializer =
         new OpenTelemetryAppenderInitializer(OpenTelemetry.noop());
+    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    OpenTelemetryAppender appender = new OpenTelemetryAppender();
+    appender.setContext(context);
+    appender.start();
+    LoggingEvent event = new LoggingEvent(
+        getClass().getName(),
+        context.getLogger(getClass()),
+        Level.INFO,
+        "OpenTelemetry compatibility check",
+        null,
+        null);
 
-    assertThatCode(initializer::afterPropertiesSet).doesNotThrowAnyException();
+    assertThatCode(() -> {
+      initializer.afterPropertiesSet();
+      appender.doAppend(event);
+    }).doesNotThrowAnyException();
+
+    appender.stop();
   }
 
 }
